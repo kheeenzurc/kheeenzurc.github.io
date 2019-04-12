eb987c16c2a87d91e78028709b6429aae39c8f68
Khen Cruz <Kheeenzurc@gmail.com>
2019-04-13 05:11:16 +0800

    Version 3.1.1

---
+++ version 3/facial-recognition/server/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const Pusher = require("pusher");
+
+// create express application
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// initialize pusher
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: 'ap1',
+  encrypted: true
+});
+
+//  create application routes
+app.post("/callback", function(req, res) {
+  // now that we are here just go ahead and then
+  console.log(req.body.emotion);
+  pusher.trigger("emotion_channel", "new_emotion", {
+    emotion: req.body.emotion
+  });
+
+  return res.json({ status: true });
+});
+
+app.listen("3128", function(){
+  console.log("server started . . .")
+});
